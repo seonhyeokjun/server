b2338ff646c2cde0db9ed252b8793f3acccb898d
seonhyeokjun <tjsrmatn@naver.com>
2024-08-03 22:48:17 +0900

    서블릿 컨테이너 초기화1

---
+++ src/main/java/hello/container/MyContainerInitV1.java
@@ -0,0 +1,16 @@
+package hello.container;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+
+import java.util.Set;
+
+public class MyContainerInitV1 implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
+        System.out.println("MyContainerInitV1.onStartup");
+        System.out.println("MyContainerInitV1 set = " + set);
+        System.out.println("MyContainerInitV1 servletContext = " + servletContext);
+    }
+}
